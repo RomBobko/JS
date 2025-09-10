6b7549e1cea03761eb7cba7f6c0c495eaf0a5240
Roman BOBKO <147614380+RomBobko@users.noreply.github.com>
2025-09-10 20:46:16 +0300

    methods js examples

---
+++ js/practice.js
@@ -0,0 +1,54 @@
+// const numbers = [5, 12, 8, 130, 44];
+
+// const greaterThanTen = numbers.filter(num => num > 10);
+// const total = numbers.reduce((acc, num) => acc + num, 0);
+// const quad = numbers.map(num => num ** 2);
+
+// console.log(greaterThanTen);
+// console.log(total);
+// console.log(quad);
+
+// const numbers = [5, 12, 8, 130, 44];
+
+// const result = numbers
+//     .filter(num => num > 10)
+//     .map(num => num ** 2)
+//     .reduce((acc, num) => acc + num, 0);
+
+// console.log(result);
+
+// const users = [
+//     { name: 'Alice', age: 25, isActive: true },
+//     { name: 'Bob', age: 30, isActive: false },
+//     { name: 'Charlie', age: 35, isActive: true },
+//     { name: 'Diana', age: 20, isActive: false },
+// ];
+
+// const result = users
+//     .filter(user => user.isActive)
+//     .map(user => user.age ** 2)
+//     .reduce((acc, age) => acc + age, 0);
+
+// console.log(result);
+
+
+// const numbers = [2, 5, 10, 7, 3];
+
+// const total = numbers.reduce((acc, num) => acc + (num % 2 !== 0 ? num : 0), 0);
+
+// console.log(total);
+
+
+// const fruits = ['apple', 'banana', 'kiwi', 'mango'];
+
+// const result = fruits.filter(fruit => (fruit.length > 4));
+
+// console.log(result);
+
+
+// const flags = [true, false, true, true, false];
+
+// const result = flags.filter(flag => flag).length;
+// console.log(result);
+
+
